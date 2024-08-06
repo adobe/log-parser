7279fc8aed311eefde238f89e39db41c50c55618
baubakg <development@gandomi.com>
2024-08-06 21:54:52 +0200

    added enrichment test headers

--- src/test/java/com/adobe/campaign/tests/logparser/core/EnrichmentTests.java
+++ src/test/java/com/adobe/campaign/tests/logparser/core/EnrichmentTests.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2022 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it.
+ */
 package com.adobe.campaign.tests.logparser.core;
 
 import org.hamcrest.Matcher;
